23dfcfcb221b50e7b250f83049546497e8b840a0
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-16 05:26:19 +0300

    yorum eklendi
    yorum satırları eklendi

--- MukemmelSayi.java
+++ MukemmelSayi.java
@@ -3,9 +3,15 @@ public class MukemmelSayi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // sayımızı number değişkenine atıyoruz
+
         System.out.print("Bir sayı giriniz:  ");
         int number = input.nextInt();
         int sum = 0;
+
+        // for dongusuyle sayının kendisi hariç 1den başlayarak tüm pozitif sayılara tam bolenlerini sum
+        // değişkeninde topluyoruz ve sayının kendisiyle karşılaştırıyoruz
+        
         for(int i = 1; i < number;i++){
             if(number%i==0) sum += i;
         }
